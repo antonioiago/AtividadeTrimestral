bfd64b241182eeddf5a6e179e1cd8916d7dd3dea
antonioiago <antonio.portela@etec.sp.gov.br>
2025-09-17 17:55:49 -0300

    Primeiro Upload

---
+++ Trabalho_Jeferson_Antonio/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_jeferson_antonio
+{
+    internal class Pessoa
+    {
+        protected string nome;
+        protected int cpf;
+        protected int idade;
+        protected string gênero;
+        protected string endereço;
+    }
+
+}
---
+++ Trabalho_Jeferson_Antonio/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_jeferson_antonio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Menu de Escolha: \n");
+            Console.WriteLine("1. Cadastrar Paciente na fila \n");
+            Console.WriteLine("2. Listar os Pacientes da fila\n");
+            Console.WriteLine("3. Atender os pacientes da fila.\n");
+            Console.WriteLine("4. Alterar os dados cadastrais do paciente.\n");
+
+        }
+    }
+}
