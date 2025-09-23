b52e58450962f78ed819b8832db182d8bc130d14
antonioiago <antonio.portela@etec.sp.gov.br>
2025-09-23 14:16:06 -0300

    Update Program.cs
    Unir os console.WrireLine

--- Trabalho_Jeferson_Antonio/Program.cs
+++ Trabalho_Jeferson_Antonio/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -11,12 +11,7 @@ namespace Trabalho_jeferson_antonio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Menu de Escolha: \n");
-            Console.WriteLine("1. Cadastrar Paciente na fila \n");
-            Console.WriteLine("2. Listar os Pacientes da fila\n");
-            Console.WriteLine("3. Atender os pacientes da fila.\n");
-            Console.WriteLine("4. Alterar os dados cadastrais do paciente.\n");
-
+            Console.WriteLine("Menu de Escolha: \n 1. Cadastrar Paciente na fila \n 2. Listar os Pacientes da fila\n 3. Atender os pacientes da fila.\n 4. Alterar os dados cadastrais do paciente.\n ");
         }
     }
-}
+
