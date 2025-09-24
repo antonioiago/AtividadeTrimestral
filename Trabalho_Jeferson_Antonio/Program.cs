92f2c089f7b4588286a464920f82978568d3a64f
antonioiago <antonio.portela@etec.sp.gov.br>
2025-09-23 22:55:34 -0300

    Update Program.cs
    Detalhes finais do código do program

--- Trabalho_Jeferson_Antonio/Program.cs
+++ Trabalho_Jeferson_Antonio/Program.cs
@@ -1,17 +1,31 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Trabalho_jeferson_antonio
+namespace projetohosp
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Menu de Escolha: \n 1. Cadastrar Paciente na fila \n 2. Listar os Pacientes da fila\n 3. Atender os pacientes da fila.\n 4. Alterar os dados cadastrais do paciente.\n ");
+            Paciente hospital = new Paciente();
+            string opcao = "";
+
+            while (opcao != "q")
+            {
+                Console.WriteLine("1 - Cadastrar");
+                Console.WriteLine("2 - Listar");
+                Console.WriteLine("3 - Atender");
+                Console.WriteLine("4 - Alterar");
+                Console.WriteLine("q - Sair");
+                Console.Write("Opcao: ");
+                opcao = Console.ReadLine();
+
+                if (opcao == "1") hospital.CadastrarPaciente();
+                else if (opcao == "2") hospital.ListarPacientes();
+                else if (opcao == "3") hospital.AtenderPaciente();
+                else if (opcao == "4") hospital.AlterarPaciente();
+                else if (opcao == "q") Console.WriteLine("Saindo...");
+                else Console.WriteLine("Opcao invalida");
+            }
         }
     }
-
+}
